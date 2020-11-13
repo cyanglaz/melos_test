b5ba05d862fae7d20b4a8ddf7512bc4b3a8a80d2
Chris Yang <ychris@google.com>
2020-11-13 10:57:20 -0800

    fix test

--- packages/conventional_commits_test_2/lib/conventional_commits_test_2.dart
+++ packages/conventional_commits_test_2/lib/conventional_commits_test_2.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 class ConventionalCommitsTest_2 {
   static const MethodChannel _channel =
-      const MethodChannel('conventional_commits_test_2_3');
+      const MethodChannel('conventional_commits_test_2');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
