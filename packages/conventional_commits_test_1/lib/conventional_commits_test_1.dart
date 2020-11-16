e35c30c4f809f2581fc946ebf7e4d9eb313b4ace
Chris Yang <ychris@google.com>
2020-11-16 14:22:06 -0800

    fix(conventional_commits_test_1):revert channel var

--- packages/conventional_commits_test_1/lib/conventional_commits_test_1.dart
+++ packages/conventional_commits_test_1/lib/conventional_commits_test_1.dart
@@ -3,11 +3,11 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ConventionalCommitsTest_1 {
-  static const MethodChannel _channel2 =
+  static const MethodChannel _channel =
       const MethodChannel('conventional_commits_test_1');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel2.invokeMethod('getPlatformVersion');
+    final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 }
