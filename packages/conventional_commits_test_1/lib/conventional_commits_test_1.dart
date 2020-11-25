97ff10fda79bca096c27ef22b47591575944ab26
Chris Yang <ychris@google.com>
2020-11-25 09:12:15 -0800

    fix(multiple): test3 (#2)
    * rename
    
    * chore(release): publish packages
    
     - conventional_commits_test_1@0.0.2
     - conventional_commits_test_2@0.0.2
    
    * fix(conventional_commits_test_1):revert channel var
    
    * fix(conventional_commits_test_1): revert channel var
    
    * fix
    
    * melos version
    
    * update
    
    * add push to update version
    
    * push
    
    * test
    
    * git log

--- packages/conventional_commits_test_1/lib/conventional_commits_test_1.dart
+++ packages/conventional_commits_test_1/lib/conventional_commits_test_1.dart
@@ -3,11 +3,11 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ConventionalCommitsTest_1 {
-  static const MethodChannel _channel =
+  static const MethodChannel _channel2 =
       const MethodChannel('conventional_commits_test_1');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+    final String version = await _channel2.invokeMethod('getPlatformVersion');
     return version;
   }
 }
