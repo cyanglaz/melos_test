df6987c3ca3349d1ab3b3b34a7bdd9fec0b7a028
Chris Yang <ychris@google.com>
2020-12-11 10:56:41 -0800

    fix(some) update channel to channel 3 (#9)

--- packages/conventional_commits_test_1/lib/conventional_commits_test_1.dart
+++ packages/conventional_commits_test_1/lib/conventional_commits_test_1.dart
@@ -3,11 +3,11 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ConventionalCommitsTest_1 {
-  static const MethodChannel channel2 =
+  static const MethodChannel channel3 =
       const MethodChannel('conventional_commits_test_1');
 
   static Future<String> get platformVersion async {
-    final String version = await channel2.invokeMethod('getPlatformVersion');
+    final String version = await channel3.invokeMethod('getPlatformVersion');
     return version;
   }
 }
