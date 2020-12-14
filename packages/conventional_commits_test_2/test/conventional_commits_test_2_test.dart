f150063f65b90fb620e8cfde1e637309da1f757a
Chris Yang <ychris@google.com>
2020-12-14 13:10:32 -0800

    fix(conventional_commits_test_2): update tests 3

--- packages/conventional_commits_test_2/test/conventional_commits_test_2_test.dart
+++ packages/conventional_commits_test_2/test/conventional_commits_test_2_test.dart
@@ -9,7 +9,7 @@ void main() {
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '44';
+      return '45';
     });
   });
 
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await ConventionalCommitsTest_2.platformVersion, '44');
+    expect(await ConventionalCommitsTest_2.platformVersion, '45');
   });
 }
