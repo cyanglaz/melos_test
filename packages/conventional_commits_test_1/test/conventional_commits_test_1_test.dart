c841a451af37396566736159ed13b2a16955d76b
Chris Yang <ychris@google.com>
2021-01-12 12:21:19 -0800

    fix(cc1): test to 51

--- packages/conventional_commits_test_1/test/conventional_commits_test_1_test.dart
+++ packages/conventional_commits_test_1/test/conventional_commits_test_1_test.dart
@@ -9,7 +9,7 @@ void main() {
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '50';
+      return '51';
     });
   });
 
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await ConventionalCommitsTest_1.platformVersion, '50');
+    expect(await ConventionalCommitsTest_1.platformVersion, '51');
   });
 }
