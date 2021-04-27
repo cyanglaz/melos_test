5e5b243b948df95223b141048b61df1cadad282f
Chris Yang <ychris@google.com>
2021-04-27 11:52:23 -0700

    remove random test

--- packages/conventional_commits_test_1/test/conventional_commits_test_1_test.dart
+++ packages/conventional_commits_test_1/test/conventional_commits_test_1_test.dart
@@ -19,10 +19,10 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    var rand = Random();
-    var n = rand.nextInt(5);
-    expect(n, 0);
-    expect(await ConventionalCommitsTest_1.platformVersion, '51');
-  });
+  // test('getPlatformVersion', () async {
+  //   var rand = Random();
+  //   var n = rand.nextInt(5);
+  //   expect(n, 0);
+  //   expect(await ConventionalCommitsTest_1.platformVersion, '51');
+  // });
 }
